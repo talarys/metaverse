7035c263bed0131e10a15cc0f77a15d8d6bce61a
talarys <d.simon.ssx0@gmail.com>
2023-07-27 22:31:45 +0200

    Boilerplate

--- app/page.js
+++ app/page.js
@@ -1,3 +1,29 @@
+import {
+  About,
+  Explore,
+  Feedback,
+  GetStarted,
+  Hero,
+  Insights,
+  WhatsNew,
+  World,
+} from '../sections'
+
+import { Footer, Navbar } from '../components'
+
 export default function Home() {
-  return <main>hello</main>
+  return (
+    <main>
+      <Navbar />
+      <Hero />
+      <About />
+      <Explore />
+      <GetStarted />
+      <WhatsNew />
+      <World />
+      <Insights />
+      <Feedback />
+      <Footer />
+    </main>
+  )
 }
