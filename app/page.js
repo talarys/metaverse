fe85c2a5298ae02f03faf9e072f53f1e670f3efb
talarys <d.simon.ssx0@gmail.com>
2023-08-02 17:38:50 +0200

    Added all gradeints

--- app/page.js
+++ app/page.js
@@ -13,18 +13,33 @@ import {Footer, Navbar} from '../components'
 
 export default function Home() {
 	return (
-		<main className='bg-primary-black overflow-hidden h-screen text-white'>
+		<main className='bg-primary-black overflow-hidden text-white'>
 			<div className='max-w-[1360px] mx-auto' >
 				<Navbar />
+
 				<Hero />
-				<About />
-				{/* <Explore /> */}
-				{/* <GetStarted /> */}
-				{/* <WhatsNew /> */}
-				{/* <World /> */}
-				{/* <Insights /> */}
-				{/* <Feedback /> */}
-				{/* <Footer /> */}
+
+				<div className='relative'>
+					<About />
+					<div className='gradient-03 z-0'/>
+					<Explore />
+				</div>
+
+				<div className='relative'>
+					<GetStarted />
+					<div className='gradient-04 z-0'/>
+					<WhatsNew />
+				</div>
+
+				<World />
+
+				<div className='relative'>
+					<Insights />
+					<div className='gradient-04 z-0'/>
+					<Feedback />
+				</div>
+
+				<Footer />
 			</div>
 		</main>
 	)
