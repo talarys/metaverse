226687aa90386aa5817ca47d0dc4b040d13f979a
talarys <d.simon.ssx0@gmail.com>
2023-08-02 15:38:44 +0200

    Minor formatting

--- sections/Hero.jsx
+++ sections/Hero.jsx
@@ -5,37 +5,38 @@ import {motion} from 'framer-motion'
 import {slideIn, staggerContainer, textVariant} from '@/utils/motion'
 
 const Hero = () => (
-	<section className='yPaddings sm:pl-16 pl-6'>
+	<section className={'yPaddings sm:pl-16 pl-6'}>
 		<motion.div
 			variants={staggerContainer}
 			initial='hidden'
 			whileInView='show'
-			viewport={{once: false, amount: 0.25}}>
-			<div
-				className='flex justify-center items-center flex-col relative z-10'>
+			viewport={{once: false, amount: 0.25}}
+			className={'innerWidth mx-auto flex flex-col'}
+		>
+			<div className='flex justify-center items-center flex-col relative z-10'>
 				<motion.h1
-					variants={textVariant(1.1)}
-					className='heroHeading'>
-				Metaverse
+					variants={textVariant(1.1)} className={'heroHeading'}>
+          Metaverse
 				</motion.h1>
 				<motion.div
 					variants={textVariant(1.2)}
-					className='flex flex-row justify-center items-center'>
-					<h1 className='heroHeading'>MA</h1>
-					<h1 className='heroDText'/>
-					<h1 className='heroHeading'>NESS</h1>
+					className='flex flex-row justify-center items-center'
+				>
+					<h1 className='heroHeading'>Ma</h1>
+					<div className='heroDText' />
+					<h1 className='heroHeading'>Ness</h1>
 				</motion.div>
 			</div>
 
-			<motion.div variants={slideIn('right', 'tween', 0.2, 1)}
-				className='relative w-full md:-mt-[40px] -mt-[12px]'>
-
-				<div className='absolute w-full h-[300px] hero-gradient rounded-tl-[140px] z-0 -top-[30px]'/>
+			<motion.div
+				variants={slideIn('right', 'tween', 0.2, 1)}
+				className='relative w-full md:-mt-[20px] -mt-[12px]'
+			>
+				<div className='absolute w-full h-[300px] hero-gradient rounded-tl-[140px] z-[0] -top-[30px]' />
 
 				<img
 					src='/cover.png'
-					alt='hero'
-					fill={true}
+					alt='hero_cover'
 					className='w-full sm:h-[500px] h-[350px] object-cover rounded-tl-[140px] z-10 relative'
 				/>
 
@@ -44,12 +45,10 @@ const Hero = () => (
 						<img
 							src='/stamp.png'
 							alt='stamp'
-							fill={true}
-							className='sm:w-[155] w-[100px] sm:h-[155px] h-[100px] object-contain'
+							className='sm:w-[155px] w-[100px] sm:h-[155px] h-[100px] object-contain'
 						/>
 					</div>
 				</a>
-
 			</motion.div>
 		</motion.div>
 	</section>
