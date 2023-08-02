cbb74ba39e93c72b758a50251a4fb4fd8dd43fa2
talarys <d.simon.ssx0@gmail.com>
2023-08-02 21:07:37 +0200

    Unused import

--- components/ExploreCard.jsx
+++ components/ExploreCard.jsx
@@ -2,7 +2,7 @@
 
 import {motion} from 'framer-motion'
 
-import {navVariants, fadeIn} from '@/utils/motion'
+import {fadeIn} from '@/utils/motion'
 
 const ExploreCard = ({id, imgUrl, title, index, active, handleClick}) => (
 	<motion.div
