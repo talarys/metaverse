cf8e738e0c8126ed0a05c3204a62b6305c4602c1
talarys <d.simon.ssx0@gmail.com>
2023-10-15 17:07:49 +0200

    Added some global gradients

--- app/page.js
+++ app/page.js
@@ -21,13 +21,13 @@ export default function Home() {
 
 				<div className='relative'>
 					<About />
-					<div className='gradient-03 z-0'/>
+					<div className='gradient-03 z-0' />
 					<Explore />
 				</div>
 
 				<div className='relative'>
 					<GetStarted />
-					<div className='gradient-04 z-0'/>
+					<div className='gradient-04 z-0' />
 					<WhatsNew />
 				</div>
 
@@ -35,7 +35,7 @@ export default function Home() {
 
 				<div className='relative'>
 					<Insights />
-					<div className='gradient-04 z-0'/>
+					<div className='gradient-04 z-0' />
 					<Feedback />
 				</div>
 
